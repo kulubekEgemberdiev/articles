a802cd9a34e03ab5d8fd35c0dbfa43208ae5a83e
KulubekEgemberdiev <askatovich61.@gmail.com>
2022-09-16 01:14:41 +0600

    Added LikesView. Added path in urls.

--- source/webapp/views/articles.py
+++ source/webapp/views/articles.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.auth.models import Permission
 from django.db.models import Q
+from django.http import JsonResponse
 from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse_lazy
 
@@ -106,3 +107,14 @@ class DeleteArticle(PermissionRequiredMixin, DeleteView):
             return self.delete(request, *args, **kwargs)
         else:
             return self.get(request, *args, **kwargs)
+
+
+class LikesView(LoginRequiredMixin, View):
+    def get(self, request, *args, pk, **kwargs):
+        article = Article.objects.get(pk=pk)
+        user = self.request.user
+        if user in article.likes.all():
+            article.likes.remove(user)
+        else:
+            article.likes.add(user)
+        return JsonResponse({"count": article.likes.count()})
