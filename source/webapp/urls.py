9859217d2a5f571c09dfa7fc5563cc75b7f5410f
KulubekEgemberdiev <askatovich61.@gmail.com>
2022-09-16 10:55:41 +0600

    Added url to comments like.

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.views.generic import RedirectView
 
 from webapp.views import IndexView, CreateArticle, ArticleView, UpdateArticle, DeleteArticle, CreateCommentView, \
-    UpdateComment, DeleteComment, LikesView
+    UpdateComment, DeleteComment, LikesView, CommentLikeView
 
 app_name = "webapp"
 
@@ -16,5 +16,6 @@ urlpatterns = [
     path('article/<int:pk>/comment/add/', CreateCommentView.as_view(), name="article_comment_create"),
     path('comments/<int:pk>/update/', UpdateComment.as_view(), name="update_comment"),
     path('comments/<int:pk>/delete/', DeleteComment.as_view(), name="delete_comment"),
+    path('comment/<int:pk>/likes/', CommentLikeView.as_view(), name='likes_comment'),
     path('article/<int:pk>/likes/', LikesView.as_view(), name='likes'),
 ]
